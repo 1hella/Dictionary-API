e395168ac040b531f41261ddf547f0a4f63d3aa8
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-02-26 00:39:48 -0800

    return dictionary definition from dictionary file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -8,10 +9,16 @@ def home():
     return render_template('home.html')
 
 
+df = pd.read_csv("dictionary.csv")
+
+
 @app.route('/api/v1/<word>')
 def test(word):
+    definition = df.loc[df['word'] == word]['definition'].squeeze()
+    if type(definition) is not str:
+        return {"error": f"{word} is not in our dictionary"}
     return {"word": word,
-            "definition": word.title()}
+            "definition": definition}
 
 
 if __name__ == '__main__':
